adae3dcff17c17abd374c2c25b632beec0a9d046
abhijith <xperio135@gmail.com>
2024-02-10 10:54:34 +0530

    updated

--- TrafficLight.java
+++ TrafficLight.java
@@ -1,79 +1,56 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.UIManager.LookAndFeelInfo;
 class TrafficLight implements ActionListener
 {
     JFrame jfrm;
-    JPanel panel;
-    String msg = "";
-    JLabel jl;
     JTextField jtf;
     JRadioButton r1,r2,r3;
     ButtonGroup bg;
-    public TrafficLight()
+    TrafficLight()
     {
-        jfrm = new JFrame("Traffic Light");
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        try {
-            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (Exception e) {}
-        jl = new JLabel("Traffic Light");
+        jfrm = new JFrame("Calculator");
+        jfrm.setLayout(new FlowLayout());
+        jfrm.setVisible(true);
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jtf = new JTextField(10);
-        jtf.setHorizontalAlignment(JTextField.CENTER);
-        jtf.setFont(new Font("Serif",Font.BOLD,20));
         jtf.setEditable(false);
         r1 = new JRadioButton("RED");
         r2 = new JRadioButton("GREEN");
         r3 = new JRadioButton("YELLOW");
-        bg = new ButtonGroup();
-        panel.add(jl);
-        panel.add(jtf);
-        panel.add(r1);
-        panel.add(r2);
-        panel.add(r3);
-        bg.add(r1);
-        bg.add(r2);
-        bg.add(r3);
         r1.addActionListener(this);
         r2.addActionListener(this);
         r3.addActionListener(this);
-        jfrm.add(panel);
-        jfrm.pack();
-        jfrm.setSize(400,400);
-        jfrm.setVisible(true);
-        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bg = new ButtonGroup();
+        bg.add(r1);
+        bg.add(r2);
+        bg.add(r3);
+        jfrm.add(jtf);
+        jfrm.add(r1);
+        jfrm.add(r2);
+        jfrm.add(r3);
     }
     public void actionPerformed(ActionEvent e)
+    {
+        String s = e.getActionCommand();
+        if(s.equals("RED"))
         {
-            msg = e.getActionCommand();
-            if(msg.equals("RED"))
-            {
-                panel.setBackground(Color.RED);
-                jtf.setBackground(Color.red);
-                jtf.setText("STOP");
-            }
-            else if(msg.equals("GREEN"))
-            {
-                panel.setBackground(Color.GREEN);
-                jtf.setBackground(Color.green);
-                jtf.setText("GO");
-            }
-            else
-            {
-                panel.setBackground(Color.YELLOW);
-                jtf.setBackground(Color.yellow);
-                jtf.setText("WAIT");
-            }
+            jfrm.getContentPane().setBackground(Color.RED);
+            jtf.setText("STOP");
         }
+        if(s.equals("GREEN"))
+        {
+            jfrm.getContentPane().setBackground(Color.GREEN);
+            jtf.setText("GO");
+        }
+        if(s.equals("YELLOW"))
+        {
+            jfrm.getContentPane().setBackground(Color.YELLOW);
+            jtf.setText("WAIT");
+        }
+    }
     public static void main(String[] args)
     {
-        TrafficLight trafficLight  = new TrafficLight();
+        TrafficLight tf = new TrafficLight();
     }
 }
