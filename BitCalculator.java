46b7da72861c6fadb166129c0dc51b495165964e
abhijith <xperio135@gmail.com>
2024-02-10 19:02:47 +0530

    intial commit

---
+++ BitCalculator.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class BitCalculator implements ActionListener
+{
+    JFrame jfrm;
+    JRadioButton r1,r2;
+    JTextField jtf;
+    JButton b;
+    BitCalculator()
+    {
+        jfrm = new JFrame();
+        jfrm.setLayout(new FlowLayout());
+        r1 = new JRadioButton("bit-1");
+        r2 = new JRadioButton("bit-2");
+        jtf = new JTextField(10);
+        jtf.setEditable(false);
+        b = new JButton("reset");
+        r1.addActionListener(this);
+        r2.addActionListener(this);
+        b.addActionListener(this);
+        jfrm.add(jtf);
+        jfrm.add(r1);
+        jfrm.add(r2);
+        jfrm.add(b);
+        jfrm.setVisible(true);
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==r1 || e.getSource()==r2)
+        {
+            if(r1.isSelected() && r2.isSelected())
+            {
+                jtf.setText("3");
+            }
+            else if(r1.isSelected())
+            {
+                jtf.setText("2");
+            }
+            else if(r2.isSelected())
+            {
+                jtf.setText("1");
+            }
+            else
+            {
+                jtf.setText("0");
+            }
+        }
+        else if(e.getSource()==b)
+        {
+            r1.setSelected(false);
+            r2.setSelected(false);
+            jtf.setText("");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Q q = new Q();
+    }
+}
